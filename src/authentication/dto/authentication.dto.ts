4b6ebca68d3873b887f32656ff648f39147b7c24
Muhammad Sohail <sohailsaihail125@gmail.com>
2021-12-02 13:58:17 +0500

    add validations in login sign Api

--- src/authentication/dto/authentication.dto.ts
+++ src/authentication/dto/authentication.dto.ts
@@ -1,20 +1,31 @@
+import { IsNotEmpty } from "class-validator";
+
 export class ResgistrationType {
+    @IsNotEmpty()
     firstName: string;
+
+    @IsNotEmpty()
     lastName: string;
+
+    @IsNotEmpty()
     userName: string;
+
+    @IsNotEmpty()
     password: string;
 }
 
 export class ResponseResgistrationType {
-    _id:string;
+    _id: string;
     firstName: string;
     lastName: string;
     userName: string;
-    hash?:string
+    hash?: string
 
 }
 
-export class loginType{
-    userName:string;
-    password:string
+export class loginType {
+    @IsNotEmpty()
+    userName: string;
+    @IsNotEmpty()
+    password: string
 }
